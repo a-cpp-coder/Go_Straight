58f42578592d198b8424cd1c6aa2f27fbf783456
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-28 04:04:31 +0700

    1

---
+++ map_function.py
@@ -0,0 +1,12 @@
+numbers = [0, 2, 4, 6, 8]
+
+def square(x):
+    return x ** 2
+test = map(square, numbers)
+print(list(test))
+
+# case 2
+numbers1 = [0, 2, 4, 6, 8]
+numbers2 = [1, 3, 5, 7, 9]
+test2 = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(test2))
