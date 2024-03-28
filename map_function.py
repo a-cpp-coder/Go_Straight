8f8de4b504f47faf99cabee154ad441053fd985e
a_cpp_coder <docdaodoi1@gmail.com>
2024-03-28 15:57:47 +0700

    Lambda is also an object(as a function), 1st class object

--- map_function.py
+++ map_function.py
@@ -10,3 +10,6 @@ numbers1 = [0, 2, 4, 6, 8]
 numbers2 = [1, 3, 5, 7, 9]
 test2 = map(lambda x, y: x + y, numbers1, numbers2)
 print(list(test2))
+# or
+add = lambda x, y: x + y
+test3 = map(add, numbers1, numbers2)
